29cdf5fe0a7cf9a40089760bd26fe10188f3c6a3
PJColombo <paulojcd2@outlook.com>
2020-11-06 05:22:22 +0100

    Action button component for the task cards

---
+++ src/components/Buttons/ActionButton.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Button } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  actionButton: {
+    background: ({ color }) => color,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+}))
+
+function ActionButton({ label, color, fullWidth = false, onClick }) {
+  const { actionButton } = useStyles({ color })
+  return (
+    <Button
+      variant="contained"
+      className={actionButton}
+      onClick={onClick}
+      fullWidth={fullWidth}
+    >
+      {label}
+    </Button>
+  )
+}
+
+ActionButton.propTypes = {
+  label: PropTypes.string,
+  color: PropTypes.string,
+}
+
+export default ActionButton
