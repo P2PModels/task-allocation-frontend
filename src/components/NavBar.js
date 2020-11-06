1790e664d9017f7dcbaff0c7c2ab68e6dd1153d2
PJColombo <paulojcd2@outlook.com>
2020-11-06 05:27:31 +0100

    NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { AppBar } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+import Logo from './Logo'
+
+import AccountModule from 'components/AccountModule/AccountModule'
+
+const useStyles = makeStyles(theme => ({
+  root: {},
+  navBar: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  scrollWrapper: {
+    position: 'fixed',
+    bottom: `${theme.spacing(2)}px`,
+    top: `${theme.spacing(2)}px`,
+  },
+  buttonWrapper: {
+    marginRight: theme.spacing(4),
+  },
+}))
+
+const NavBar = () => {
+  const { navBar, buttonWrapper } = useStyles()
+  return (
+    <div>
+      <AppBar className={navBar} position="fixed" color="default" elevation={3}>
+        <Logo />
+        <div className={buttonWrapper}>
+          <AccountModule />
+        </div>
+      </AppBar>
+    </div>
+  )
+}
+
+export default NavBar
