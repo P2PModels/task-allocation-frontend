3639744f0d9066942c1aa98deccec2168fec6c44
PJColombo <paulojcd2@outlook.com>
2020-11-09 02:03:46 +0100

    App state implemented

---
+++ src/contexts/AppState.js
@@ -0,0 +1,49 @@
+// TODO: Build up app state
+import React, { createContext, useContext } from 'react'
+import useOrgData from '../hooks/useOrgData'
+// import useUser from '../hooks/useUser'
+
+const AppStateContext = createContext()
+
+export function AppStateProvider({ appName, children }) {
+  const {
+    config,
+    errors,
+    installedApps,
+    loadingAppData,
+    roundRobinConnector,
+  } = useOrgData(appName)
+
+  // const { userErrors, user, loadingUserData } = useUser(
+  //   userId,
+  //   roundRobinConnector
+  // )
+
+  const appLoading =
+    !errors && /* !userErrors */ loadingAppData /*  && loadingUserData */
+
+  return (
+    <AppStateContext.Provider
+      value={{
+        config,
+        errors: { orgErrors: errors /* userErrors */ },
+        installedApps,
+        isLoading: appLoading,
+        roundRobinConnector,
+        /* user, */
+      }}
+    >
+      {children}
+    </AppStateContext.Provider>
+  )
+}
+
+export function useAppState() {
+  const context = useContext(AppStateContext)
+
+  if (!context) {
+    throw new Error('useAppState must be used within an AppStateProvider')
+  }
+
+  return context
+}
