7e70a369feeb010de54bf316b83140d8751ccbef
PJColombo <paulojcd2@outlook.com>
2020-11-10 13:09:28 +0100

    Transaction Modal disabled when prompting Metamask to sign tx

--- src/components/Modals/TransactionModal.js
+++ src/components/Modals/TransactionModal.js
@@ -6,8 +6,8 @@ import DialogTitle from '@material-ui/core/DialogTitle'
 import IconButton from '@material-ui/core/IconButton'
 import CloseIcon from '@material-ui/icons/Close'
 import InfoOutlined from '@material-ui/icons/InfoOutlined'
-import { makeStyles, useTheme } from '@material-ui/core/styles'
-import { Button, DialogActions } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import { Button, CircularProgress, DialogActions } from '@material-ui/core'
 
 const useStyles = makeStyles(theme => ({
   closeButton: {
@@ -30,40 +30,72 @@ const useStyles = makeStyles(theme => ({
   operationTitle: {
     fontWeight: 700,
   },
+  opacityWrapper: {
+    opacity: 0.5,
+  },
+  activatingProgress: {
+    position: 'absolute',
+    top: '45%',
+    left: '45%',
+  },
+  opacityBackground: {
+    backgroundColor: theme.palette.grey,
+    cursor: ({ activating }) => (activating ? '' : 'pointer'),
+  },
 }))
 
-const TransactionModal = ({ modalContent, open, onClose }) => {
-  const { title, message, createTransactionHandler } = modalContent
-  const { titleWrapper, operationTitle, modalTitle, closeButton } = useStyles()
+const TransactionModal = ({
+  modalContent,
+  activating = true,
+  open,
+  onClose,
+}) => {
+  const { title, message, createTransactionHandler } = modalContent || {}
+  const {
+    titleWrapper,
+    operationTitle,
+    modalTitle,
+    closeButton,
+    opacityWrapper,
+    activatingProgress,
+    opacityBackground,
+  } = useStyles(activating)
   return (
     <Dialog open={open} onClose={onClose}>
-      <DialogTitle>
-        <div className={titleWrapper}>
-          <InfoOutlined color="primary" fontSize="large" />
-          <span className={modalTitle}>
-            Perform operation: <span className={operationTitle}>{title}</span>
-          </span>
+      <div className={opacityBackground}>
+        <div className={activating ? opacityWrapper : null}>
+          <DialogTitle>
+            <div className={titleWrapper}>
+              <InfoOutlined color="primary" fontSize="large" />
+              <span className={modalTitle}>
+                Perform operation:{' '}
+                <span className={operationTitle}>{title}</span>
+              </span>
+            </div>
+            <IconButton
+              aria-label="close"
+              className={closeButton}
+              onClick={onClose}
+            >
+              <CloseIcon />
+            </IconButton>
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText>{message}</DialogContentText>
+            <DialogActions>
+              <Button
+                variant="outlined"
+                color="primary"
+                onClick={createTransactionHandler}
+                disabled={activating}
+              >
+                Create Transaction
+              </Button>
+            </DialogActions>
+          </DialogContent>
         </div>
-        <IconButton
-          aria-label="close"
-          className={closeButton}
-          onClick={onClose}
-        >
-          <CloseIcon />
-        </IconButton>
-      </DialogTitle>
-      <DialogContent>
-        <DialogContentText>{message}</DialogContentText>
-        <DialogActions>
-          <Button
-            variant="outlined"
-            color="primary"
-            onClick={createTransactionHandler}
-          >
-            Create Transaction
-          </Button>
-        </DialogActions>
-      </DialogContent>
+      </div>
+      {activating && <CircularProgress className={activatingProgress} />}
     </Dialog>
   )
 }
