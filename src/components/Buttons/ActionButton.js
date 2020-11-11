c23993b0685b289b37b92f96e0bf0a61dea78fe7
PJColombo <paulojcd2@outlook.com>
2020-11-11 06:49:51 +0100

    Lightnen and darken color function for the task action buttons

--- src/components/Buttons/ActionButton.js
+++ src/components/Buttons/ActionButton.js
@@ -2,17 +2,24 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Button } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
+import { lightenDarkenColor } from '../../helpers/css-helpers'
 
 const useStyles = makeStyles(() => ({
   actionButton: {
     background: ({ color }) => color,
     color: 'white',
     fontWeight: 'bold',
+    '&:hover': {
+      backgroundColor: ({ hoverColor }) => hoverColor,
+    },
   },
 }))
 
 function ActionButton({ label, color, fullWidth = false, onClick }) {
-  const { actionButton } = useStyles({ color })
+  const { actionButton } = useStyles({
+    color: color,
+    hoverColor: lightenDarkenColor(color, -20),
+  })
 
   return (
     <Button
