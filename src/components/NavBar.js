6d3a6bba6ba74ddbb3cb5b4f74b62f1b8b7bb69e
luishporras <luishercep@gmail.com>
2022-06-14 11:17:45 +0200

    Add container to logo, change theme spacing values, add some colors to theme

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { AppBar } from '@material-ui/core'
+import { AppBar,
+         Container
+} from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
 import Logo from './Logo'
@@ -7,34 +9,25 @@ import Logo from './Logo'
 import AccountModule from '../components/AccountModule/AccountModule'
 
 const useStyles = makeStyles(theme => ({
-  root: {},
   navBar: {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-  },
-  scrollWrapper: {
-    position: 'fixed',
-    bottom: `${theme.spacing(2)}px`,
-    top: `${theme.spacing(2)}px`,
-  },
-  buttonWrapper: {
-    marginRight: theme.spacing(4),
-  },
+  }
 }))
 
 const NavBar = () => {
   const { navBar, buttonWrapper } = useStyles()
   return (
-    <div>
-      <AppBar className={navBar} position="fixed" color="default" elevation={3}>
+    <AppBar color="default" elevation={3}>
+      <Container maxWidth="xl" className={navBar}>
         <Logo />
-        <div className={buttonWrapper}>
+        <div>
           <AccountModule />
         </div>
-      </AppBar>
-    </div>
+      </Container>
+    </AppBar>
   )
 }
 
