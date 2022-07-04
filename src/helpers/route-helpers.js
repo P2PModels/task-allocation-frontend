e403f3d4d070a15262003a4e97b81afaaf7387f2
luishporras <luishercep@gmail.com>
2022-07-04 17:02:20 +0200

    Add route helpers

--- src/helpers/route-helpers.js
+++ src/helpers/route-helpers.js
@@ -1,14 +1,18 @@
 const positionToResource = {
-  user: 1,
+    user: 1,
 }
 
 export function getResourceFromPathname(pathname, resourceName) {
-  const resources = pathname.split('/').filter(p => p.length)
+    const resources = pathname.split('/').filter(p => p.length)
 
-  const pos = positionToResource[resourceName]
-  if (pos > -1 && resources && resources.length) {
-    return resources[pos]
-  }
+    const pos = positionToResource[resourceName]
+    if (pos > -1 && resources && resources.length) {
+        return resources[pos]
+    }
 
-  return null
+    return null
+}
+
+export function generateUrl(baseUrl, param) {
+    return baseUrl[-1] === '/' ? baseUrl + param : baseUrl + '/' + param
 }
