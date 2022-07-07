953cb995ab5d1f8338cb98254adf72dbc6a8748a
luishporras <luishercep@gmail.com>
2022-07-07 11:50:35 +0200

    Add queries for each model

--- src/queries/queries.js
+++ src/queries/queries.js
@@ -1,64 +1,11 @@
 import { gql } from '@apollo/client'
-import { TaskStatuses } from '../types/taskStatuses'
 
-export const CONFIG = gql`
-    query Config($id: String!) {
-        config(id: $id) {
-            id
-            maxAllocatedTasks
-        }
-    }
-`
-
-export const USER_TASKS_ACCEPTED = gql`
-    query Tasks($userId: ID!, $first: Int!, $skip: Int!) {
-        tasks(
-            where: { status: ACCEPTED, userId: $userId }
-            first: $first
-            skip: $skip
-        ) {
-            id
-            # endDate
-            # reallocationTime
-            status
-            userId
-            # assignee {
-            #     id
-            # }
-        }
-    }
-`
-
-export const FCFS_TASKS_AVAILABLE = gql`
-    query Tasks($first: Int!, $skip: Int!) {
-        tasks(where: { status: AVAILABLE }, first: $first, skip: $skip) {
-            id
-            status
-            userId
-        }
-    }
-`
-
-export const FCFS_TASKS = gql`
-    query Tasks($first: Int!, $skip: Int!) {
-        tasks(first: $first, skip: $skip) {
-            id
-            status
-            userId
-        }
-    }
-`
-
-export const USER = gql`
-    query User($id: String!) {
-        user(id: $id) {
-            id
-            benefits
-            available
-        }
-    }
-`
-
+/**
+ * Get all users
+ *
+ * @param  {Int!} $first
+ * @param  {Int!} $skip
+ */
 export const FCFS_USERS = gql`
     query Users($first: Int!, $skip: Int!) {
         users(first: $first, skip: $skip) {
@@ -68,6 +15,11 @@ export const FCFS_USERS = gql`
     }
 `
 
+/**
+ * Get user by id
+ *
+ * @param  {String!} $id
+ */
 export const FCFS_USER = gql`
     query FCFSUser($id: String!) {
         users(id: $id) {
@@ -77,6 +29,173 @@ export const FCFS_USER = gql`
     }
 `
 
+/**
+ * Get all tasks
+ *
+ * @param  {Int!} $first
+ * @param  {Int!} $skip
+ */
+export const FCFS_TASKS = gql`
+    query Tasks($first: Int!, $skip: Int!) {
+        tasks(first: $first, skip: $skip) {
+            id
+            status
+            userId
+        }
+    }
+`
+/**
+ * Get all available tasks
+ *
+ * @param  {Int!} $first
+ * @param  {Int!} $skip
+ */
+export const FCFS_TASKS_AVAILABLE = gql`
+    query Tasks($first: Int!, $skip: Int!) {
+        tasks(where: { status: Available }, first: $first, skip: $skip) {
+            id
+            status
+            userId
+        }
+    }
+`
+/**
+ * Get tasks accepted by the user, currently only one
+ * task at a time can be accepted
+ *
+ * @param  {ID!} $userId
+ * @param  {Int!} $first
+ * @param  {Int!} $skip
+ */
+export const FCFS_USER_TASKS_ACCEPTED = gql`
+    query Tasks($userId: ID!, $first: Int!, $skip: Int!) {
+        tasks(
+            where: { status: Accepted, userId: $userId }
+            first: $first
+            skip: $skip
+        ) {
+            id
+            status
+            userId
+        }
+    }
+`
+
+/**
+ * Get all users
+ *
+ * @param  {Int!} $first
+ * @param  {Int!} $skip
+ */
+export const RR_USERS = gql`
+    query Users($first: Int!, $skip: Int!) {
+        users(first: $first, skip: $skip) {
+            id
+            benefits
+            available
+            # rejectedTasks {
+            #     id
+            # }
+        }
+    }
+`
+/**
+ * Get user by id
+ *
+ * @param  {String!} $id
+ */
+export const RR_USER = gql`
+    query RRUser($id: String!) {
+        users(id: $id) {
+            id
+            benefits
+            available
+            # rejectedTasks {
+            #     id
+            # }
+        }
+    }
+`
+/**
+ * Get all tasks
+ *
+ * @param  {Int!} $first
+ * @param  {Int!} $skip
+ */
+export const RR_TASKS = gql`
+    query Tasks($first: Int!, $skip: Int!) {
+        tasks(first: $first, skip: $skip) {
+            id
+            endDate
+            reallocationTime
+            status
+            assignee {
+                id
+            }
+        }
+    }
+`
+/**
+ * Get all tasks assigned to a user
+ *
+ * @param  {ID!} $userId
+ * @param  {Int!} $first
+ * @param  {Int!} $skip
+ */
+export const RR_USER_TASKS_ASSIGNED = gql`
+    query Tasks($userId: ID!, $first: Int!, $skip: Int!) {
+        tasks(
+            where: { status: Assigned, assignee: $userId }
+            first: $first
+            skip: $skip
+        ) {
+            id
+            endDate
+            reallocationTime
+            status
+            assignee {
+                id
+            }
+        }
+    }
+`
+/**
+ * Get all tasks accepted by the user, currently only one
+ * task at a time can be accepted
+ *
+ * @param  {ID!} $userId
+ * @param  {Int!} $first
+ * @param  {Int!} $skip
+ */
+export const RR_USER_TASKS_ACCEPTED = gql`
+    query Tasks($userId: ID!, $first: Int!, $skip: Int!) {
+        tasks(
+            where: { status: Accepted, assignee: $userId }
+            first: $first
+            skip: $skip
+        ) {
+            id
+            endDate
+            reallocationTime
+            status
+            assignee {
+                id
+            }
+        }
+    }
+`
+
+// Legacy
+export const USER = gql`
+    query User($id: String!) {
+        user(id: $id) {
+            id
+            benefits
+            available
+        }
+    }
+`
+// Legacy
 export const USER_REJECTED_TASKS = gql`
     query User($id: String!) {
         user(id: $id) {
