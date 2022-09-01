a756fc4796fac0cf5e9e2f3cde9f2230ebea83ac
luishporras <luishercep@gmail.com>
2022-09-01 13:53:05 +0200

    Make the queue restart once a new tx is added and the queue was completed, change loading state to ref to avoid errors when multiple txs are added rapidly

--- src/hooks/useQueuedTransactions.js
+++ src/hooks/useQueuedTransactions.js
@@ -5,7 +5,7 @@ import useTransaction from './useTransaction'
 export default function useQueuedTransactions() {
     const [txsHash, setTxsHash] = useState([])
     const [receipts, setReceipts] = useState([])
-    const [loading, setLoading] = useState(false)
+    const loading = useRef(false)
     const [error, setError] = useState([])
 
     const [providers, setProviders] = useState([])
@@ -13,7 +13,6 @@ export default function useQueuedTransactions() {
     const [txsToProcess, setTxsToProcess] = useState([])
     const currentTxIndex = useRef(null)
     const [preparingNextTx, setPreparingNextTx] = useState(false)
-    // const [lastTxIndex, setLastTxIndex] = useState(0)
     const lastTxIndex = useRef(0)
 
     let [
@@ -27,7 +26,9 @@ export default function useQueuedTransactions() {
 
     const queueTransaction = (web3, inputTx, usePrivateKey = false) => {
         console.log(
-            `%c[queueTransaction] Tx added to queue in position ${lastTxIndex}, ${
+            `%c[queueTransaction] Tx added to queue in position ${
+                lastTxIndex.current
+            }, ${
                 usePrivateKey
                     ? 'private key WILL be used'
                     : 'private key WONT be used'
@@ -35,29 +36,29 @@ export default function useQueuedTransactions() {
             'color: orange'
         )
 
-        // If its the first time
-        if (!loading && currentTxIndex.current == null) {
+        // If its the first time or the queue was completed
+        if (!loading.current) {
+            if (currentTxIndex.current == null) {
+                currentTxIndex.current = 0
+            }
             console.log('%c[useQueueTransaction] Bootstraping', 'color: orange')
-            setLoading(true)
-            currentTxIndex.current = 0
+            loading.current = true
             setPreparingNextTx(true)
             // Bootstrap
             processTransaction(web3, inputTx, usePrivateKey)
+            currentTxIndex.current++
         }
 
         setProviders(providers => [...providers, web3])
         setTxsToProcess(inputTxs => [...inputTxs, inputTx])
         setUsePrivateKeys(usePrivateKeys => [...usePrivateKeys, usePrivateKey])
-        // setLastTxIndex(lastTxIndex => lastTxIndex + 1)
         lastTxIndex.current++
-
-        // return lastTxIndex.current
     }
 
     const stop = () => {
         setTxsHash([])
         setReceipts([])
-        setLoading(false)
+        loading.current = false
         setError([])
         currentTxIndex.current = null
         lastTxIndex.current = 0
@@ -77,7 +78,13 @@ export default function useQueuedTransactions() {
         }
         if (currentLoading && currentTxHash) {
             console.log(
-                `%c[useQueuedTransactions] Processing tx ${currentTxHash}...`,
+                `%c[useQueuedTransactions] Processing tx ${currentTxHash.slice(
+                    0,
+                    5
+                )}...${currentTxHash.slice(
+                    currentTxHash.length - 3,
+                    currentTxHash.length
+                )}`,
                 'color: orange'
             )
             setTxsHash(txsHash => [...txsHash, currentTxHash])
@@ -87,7 +94,7 @@ export default function useQueuedTransactions() {
         if (currentReceipt && !currentLoading && !preparingNextTx) {
             console.log(
                 `%c[useQueuedTransactions] Tx ${
-                    currentTxIndex.current + 1
+                    currentTxIndex.current //+ 1
                 } processed: `,
                 'color: orange'
             )
@@ -96,22 +103,25 @@ export default function useQueuedTransactions() {
             // Update state
             setReceipts(receipts => [...receipts, currentReceipt])
             setError(error => [...error, null])
-            setPreparingNextTx(true)
 
-            if (currentTxIndex.current + 1 < lastTxIndex.current) {
+            if (currentTxIndex.current < lastTxIndex.current) {
+                setPreparingNextTx(true)
                 console.log(
                     '%c[useQueueTransaction] Sending next tx...',
                     'color: orange'
                 )
                 processTransaction(
-                    providers[currentTxIndex.current + 1],
-                    txsToProcess[currentTxIndex.current + 1],
-                    usePrivateKeys[currentTxIndex.current + 1]
+                    providers[currentTxIndex.current],
+                    txsToProcess[currentTxIndex.current],
+                    usePrivateKeys[currentTxIndex.current]
                 )
                 currentTxIndex.current++
-            } else if (currentTxIndex.current + 1 == lastTxIndex.current) {
-                // Bulk report finished
-                setLoading(false)
+            } else if (currentTxIndex.current == lastTxIndex.current) {
+                console.log(
+                    '%c[useQueueTransaction] Queue finished',
+                    'color: orange'
+                )
+                loading.current = false
             }
             return
         }
@@ -126,6 +136,6 @@ export default function useQueuedTransactions() {
     return [
         queueTransaction,
         stop,
-        { data: { txsHash, receipts }, loading, error },
+        { data: { txsHash, receipts }, loading: loading.current, error },
     ]
 }
