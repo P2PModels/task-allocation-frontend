704beb225ac98d5d48383c253c66c8e5947cb74a
luishporras <luishercep@gmail.com>
2022-11-25 11:09:41 +0100

    Build prototype

---
+++ build/precache-manifest.fdb6a0538fb7b1b5e9a192dd6d4dbb04.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73580b4ae59ddc93b6444ebef420c731",
+    "url": "/index.html"
+  },
+  {
+    "revision": "358d4f44d0388aac2b4d",
+    "url": "/static/css/2.3940c086.chunk.css"
+  },
+  {
+    "revision": "358d4f44d0388aac2b4d",
+    "url": "/static/js/2.26ab5011.chunk.js"
+  },
+  {
+    "revision": "e1094d219c9733b584860763fd6aeb55",
+    "url": "/static/js/2.26ab5011.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f70a0a487919ade81f3a",
+    "url": "/static/js/main.22a082c7.chunk.js"
+  },
+  {
+    "revision": "119296aa69dd5ef0492f",
+    "url": "/static/js/runtime-main.6c5e869a.js"
+  },
+  {
+    "revision": "885082cfeafaa1b6a5e25aaa8d2d8059",
+    "url": "/static/media/DownloadMetamask.885082cf.png"
+  },
+  {
+    "revision": "7e0db7067450dc6c30181aef29d64e95",
+    "url": "/static/media/Homepage.7e0db706.svg"
+  },
+  {
+    "revision": "11008d2b46b5c047b10e20346b6a3b85",
+    "url": "/static/media/Logo.11008d2b.svg"
+  },
+  {
+    "revision": "92a73d44a08b455c207ddd975ee713bc",
+    "url": "/static/media/MetamaskLogo.92a73d44.png"
+  },
+  {
+    "revision": "c9f7f48d277124292ff187e0466a0221",
+    "url": "/static/media/NoThumbnail.c9f7f48d.jpg"
+  },
+  {
+    "revision": "594c1aea6749d533b81df0a2317c8b07",
+    "url": "/static/media/NoVideo.594c1aea.png"
+  },
+  {
+    "revision": "66209ae01f484e46679622dd607fcbc5",
+    "url": "/static/media/roboto-100.66209ae0.ttf"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/static/media/roboto-v20-latin-300.b00849e0.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/static/media/roboto-v20-latin-300.ef7c6637.woff2"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/static/media/roboto-v20-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/static/media/roboto-v20-latin-500.87284894.woff"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/static/media/roboto-v20-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/static/media/roboto-v20-latin-700.adcde98f.woff"
+  }
+]);
